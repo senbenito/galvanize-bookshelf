6e40af2c41f7f2c23c1ace8f157cc58aa8cd7082
Shannon Rivers <shannon.rivers@gmail.com>
2017-06-05 17:07:45 -0600

    routes including POST

--- routes/books.js
+++ routes/books.js
@@ -6,5 +6,52 @@ const express = require('express');
 const router = express.Router();
 
 // YOUR CODE HERE
+const knex = require('../knex');
+const humps = require('humps');
+
+router.get('/books', (req, res, next)=>{
+  knex('books')
+  .select('id', 'title', 'author', 'genre', 'description', 'cover_url', 'created_at', 'updated_at').orderBy('title')
+  .then((result)=>{
+    res.send(humps.camelizeKeys(result));
+  });
+});
+
+router.get('/books/:id', (req, res, next)=>{
+  let id = Number.parseInt(req.params.id);
+  knex('books')
+  .select('id', 'title', 'author', 'genre', 'description', 'cover_url', 'created_at', 'updated_at').where('id', id).orderBy('title')
+  .then((result)=>{
+    res.send(humps.camelizeKeys(result[0]));
+  });
+});
+
+router.post('/books', (req, res, next)=>{
+  let body = humps.decamelizeKeys(req.body);
+  // console.log(body);
+
+  knex.insert(body)
+  .into('books')
+  .returning('*')
+  .then((response)=>{
+    res.send(humps.camelizeKeys(response[0]));
+  });
+});
+
+router.patch('/books/:id', (req, res, next)=>{
+  let id = Number.parseInt(req.params.id);
+  let body = humps.decamelizeKeys(req.body);
+  console.log(body);
+
+  knex('books')
+  // .into('books')
+  .where('id', id)
+  .update(body)
+  .returning('*')
+  .then((response)=>{
+    res.send(humps.camelizeKeys(response[0]));
+  });
+});
+
 
 module.exports = router;
--- seeds/books.js
+++ seeds/books.js
@@ -75,8 +75,11 @@ exports.seed = function(knex, Promise) {
     created_at: new Date('2016-06-26 14:26:16 UTC'),
     updated_at: new Date('2016-06-26 14:26:16 UTC')
   }];
-  return knex('books').insert(data)
+
+  return knex('books').del()
   .then(function(){
-	return knex.raw("SELECT setval('books_id_seq', (SELECT MAX(id) FROM books))")
-	});
+    return knex('books').insert(data)})
+  .then(function(){
+  	return knex.raw("SELECT setval('books_id_seq', (SELECT MAX(id) FROM books))")
+  	});
 };
